a99bd2eb8483d0e4910274e7ca53510550595e78
nutchapon <nutchapon.han@gmail.com>
2019-10-02 17:06:46 +0700

    day2 install paricle and sematic UI and create MainBG/Login

--- vssensoldemo/src/App.js
+++ vssensoldemo/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import MainBackground from './components/MainBackground/MainBackground.js'
-
+import LogInBlock from './components/LogInBlock/LogInBlock.js'
+import 'semantic-ui/dist/semantic.min.css';
 
 class App extends Component {
 
 
   render(){
     return(
-      <div>
+      <div className="App">
         <MainBackground />
+        <LogInBlock />
       </div>
     )
   }
--- vssensoldemo/src/components/MainBackground/MainBackground.js
+++ vssensoldemo/src/components/MainBackground/MainBackground.js
@@ -1,11 +1,129 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+import './MainBackground.css'
 
 const MainBackground = () => {
 
+    const config = {
+        "particles": {
+          "number": {
+            "value": 288,
+            "density": {
+              "enable": true,
+              "value_area": 1267.6506575248839
+            }
+          },
+          "color": {
+            "value": "#45784e"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 1,
+            "random": true,
+            "anim": {
+              "enable": true,
+              "speed": 1,
+              "opacity_min": 0,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 5.511524597934278,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 4,
+              "size_min": 0.3,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 3,
+            "direction": "bottom",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 1653.457379380283,
+              "rotateY": 1653.457379380283
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "bubble"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 250,
+              "size": 0,
+              "duration": 2,
+              "opacity": 0,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 400,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }
+
+
     return(
-        <div>
-            <p>Hello page</p>
-        </div>
+
+            <Particles 
+                params={config} 
+                className="particles"
+                style={{"width": "100%", "height":"100%"}}
+            />
+  
     )
 
 }
